3c13da83c55b1edf32b6237b544275e7eaeb61d2
Adam Kapica <adam.kapica@gmail.com>
2021-04-29 20:53:50 +0200

    made some bugs

--- app/Main.hs
+++ app/Main.hs
@@ -10,21 +10,43 @@ getCommand prompt = do
     hFlush stdout
     getLine
 
-checkCommand :: String -> Int
-checkCommand command = case T.splitOn (T.pack " ") (T.pack command) !! 0 of
-      T.pack "nic" -> 0
-      T.pack "cos" -> 1
-    
+lookAround :: IO()
+lookAround = putStrLn "lookAround"
 
---head :: [a] -> a
---head (x:_) = x
+pickUp :: IO()
+pickUp = putStrLn "pickUp"
+
+readNote :: IO()
+readNote = putStrLn "readNote"
+
+use :: IO()
+use = putStrLn "use"
+
+showEq :: IO()
+showEq = putStrLn "showEq"
+
+enterCode :: IO()
+enterCode = putStrLn "enterCode"
 
 main = do {
---    str <- getLine;
---    putStrLn str;
     command <- getCommand "Co robisz?";
-    checkCommand command;
+    if command == "rozgladam sie"
+      then lookAround
+      else if command == "podnosze"
+        then pickUp
+        else if command == "czytam"
+          then readNote
+          else if command == "uzywam"
+            then use
+            else if command == "przegladam eq"
+              then showEq
+              else if command == "wpisuje kod"
+                then enterCode
+                else if command == "ratunku!"
+                  then putStrLn "Co robisz? \n rozgladam sie \n podnosze \n czytam \n uzywam \n przegladam eq \n wpisuje kod"
+                  else putStrLn "Błędna komenda";
+--    print $ T.splitOn (T.pack " ") (T.pack command) !! 0;
+--    checkCommand $ T.splitOn (T.pack " ") (T.pack command) !! 0;
 --    T.splitOn (T.pack " ") (T.pack command) !! 0;
     --    T.unpack $ T.splitOn (T.pack " ") (T.pack command);
-    putStrLn command;
 }

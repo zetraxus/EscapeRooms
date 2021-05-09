e0685807c02433ca7455c1b19d01c50980175c10
PatrycjaKarbownik <patrycja@karbownik.net>
2021-05-09 21:06:53 +0200

    feature: working main loop

--- app/Main.hs
+++ app/Main.hs
@@ -2,29 +2,72 @@ module Main where
 
 import Lib
 import System.IO (hFlush, stdout)
-import Data.Text as T
---
-getCommand :: String -> IO String
-getCommand prompt = do
+import Prelude
+
+type GameState = [Int]
+
+getInputLine :: String -> IO String
+getInputLine prompt = do
     putStrLn prompt
     hFlush stdout
     getLine
 
-checkCommand :: String -> Int
-checkCommand command = case T.splitOn (T.pack " ") (T.pack command) !! 0 of
-      T.pack "nic" -> 0
-      T.pack "cos" -> 1
-    
+tokenize :: String -> [String]
+tokenize command = do
+  let splittedCommand = words command
+  splittedCommand
 
---head :: [a] -> a
---head (x:_) = x
+lookAround :: GameState -> IO()
+lookAround gameState = do
+  putStrLn "lookAround"
+  game gameState
 
-main = do {
---    str <- getLine;
---    putStrLn str;
-    command <- getCommand "Co robisz?";
-    checkCommand command;
---    T.splitOn (T.pack " ") (T.pack command) !! 0;
-    --    T.unpack $ T.splitOn (T.pack " ") (T.pack command);
-    putStrLn command;
-}
+pickUp :: GameState -> IO()
+pickUp gameState = do
+  putStrLn "pickUp"
+  game [0,0,0]
+
+readNote :: IO()
+readNote = putStrLn "readNote"
+
+use :: IO()
+use = putStrLn "use"
+
+showEq :: IO()
+showEq = putStrLn "showEq"
+
+enterCode :: IO()
+enterCode = putStrLn "enterCode"
+
+help :: IO()
+help = putStrLn "help"
+
+command :: [String] -> GameState -> IO ()
+command line gameState = do
+  let cmd = line !! 0
+  case cmd of
+    "rozgladam" -> lookAround gameState
+    "podnosze" -> pickUp gameState
+    "czytam" -> readNote
+    "uzywam" -> use
+    "przegladam" -> showEq
+    "wpisuje" -> enterCode
+    "ratunku!" -> help
+    _ -> putStrLn "Bledna komenda"
+
+initialGameState :: GameState
+initialGameState = [1, 1, 1]
+
+gameOver :: GameState -> Bool
+gameOver gameState = gameState == [0, 0, 0]
+
+game :: GameState -> IO()
+game gameState = do
+    if gameOver gameState then putStrLn "Koniec gry!"
+      else do
+        line <- getInputLine "Co robisz?";
+        let tokenizedLine = tokenize line
+        command tokenizedLine initialGameState
+
+
+main = game initialGameState
